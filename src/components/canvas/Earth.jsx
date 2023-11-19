4392fdd74b2c9d85d4f1d051d6e189544cd3d787
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2023-11-19 12:24:58 +0530

    Contact Page and 3d Phone added

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  return (
-    <div>Earth</div>
-  )
-}
+  const contact = useGLTF("./contact/scene.gltf");
 
-export default Earth
+  return (
+    <primitive object={contact.scene} scale={2.0} position-y={0} rotation-y={0} />
+  );
+};
+
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop='demand'
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 40,
+        near: 0.4,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <ambientLight intensity={5} color='white'/>
+        <directionalLight position={[5,5,7]} color='red'/>
+        <pointLight color='#7EBDE1' position={[5,5,7]} intensity={50}/>
+        <spotLight color='#7EBDE1' position={[5,5,7]} intensity={60}/>
+        
+        <Earth />
+
+        <Preload all />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default EarthCanvas;
